a3493e42320d6d4332922bfb0fc782b9eb84a780
Sadra Bigdeli <sadra.bigdeli1350@gmail.com>
2023-10-13 09:35:54 +0330

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import image1url from "./assets/images/1.png";
 import image2url from "./assets/images/2.png";
 import image3url from "./assets/images/3.png";
@@ -9,6 +9,8 @@ import image7url from "./assets/images/7.png";
 import image8url from "./assets/images/8.png";
 import "./App.css";
 import Items from "./components/Items";
+import Gameover from "./components/Gameover";
+import Countdown from "react-countdown";
 const images = [
   image1url,
   image2url,
@@ -20,43 +22,95 @@ const images = [
   image8url,
 ];
 
-function App() {
-  const [selectedItemId, setSelectedItemId] = useState([]);
-  const [items, setItems] = useState(() =>
-    Array.from({ length: 16 })
-      .map((_, i) => {
-        return {
-          id: i + 1,
-          identifier: Math.floor(i / 2),
-          image: images[Math.floor(i / 2)],
-        };
-      })
-      .sort(() => Math.random() - 0.5)
-  );
+const generateRandom = () => {
+  Array.from({ length: 16 })
+    .map((_, i) => {
+      return {
+        id: i + 1,
+        identifier: Math.floor(i / 2),
+        image: images[Math.floor(i / 2)],
+      };
+    })
+    .sort(() => Math.random() - 0.5);
+};
 
+function App() {
+  const pending = useRef(false);
+  const [move, setMove] = useState(25);
+  const [gameTime, setGameTime] = useState(5);
+  const [handleTimer, setHandleTimer] = useState(null);
+  const [selectedItemId, setSelectedItemId] = useState([]);
+
+  const [items, setItems] = useState(generateRandom);
+
+  const handleStartGame = () => {
+    const newItems = generateRandom();
+    setSelectedItemId(newItems.map((i) => i.id));
+
+    setItems(newItems);
+
+    setTimeout(() => {
+      selectedItemId(
+        newItems.map((i) => {
+          i.id;
+        })
+      );
+    }, 0);
+
+    setTimeout(() => {
+      selectedItemId([]);
+    }, 1000);
+
+
+    const timer = setInterval(() => {
+      setGameTime((t) => t - 1);
+    }, 1000);
+    setHandleTimer(timer);
+
+
+  };
+
+  useEffect(() => {
+    if (gameTime < 0) {
+      clearInterval(timer);
+    }
+  }, [gameTime]);
+
+  const handleStartAgain = () => {
+    location.reload();
+  };
   const handleClick = (item) => {
+    if (pending.current) return;
+    setMove(move - 1);
     setSelectedItemId([...selectedItemId, item.id]);
 
     if (selectedItemId.length % 2 !== 0) {
+      pending.current = true;
       const lastItemId = selectedItemId[selectedItemId.length - 1];
       const lastItem = items.find((item) => item.id === lastItemId);
 
       if (item.identifier !== lastItem.identifier) {
         setTimeout(() => {
-          setSelectedItemId(selectedItemId.filter(item => item !== lastItemId))
-        },1000)
-      } 
+          setSelectedItemId(
+            selectedItemId.filter((item) => item !== lastItemId)
+          );
+          pending.current = false;
+        }, 1000);
+      }
+    } else {
+      pending.current = false;
     }
   };
+
   return (
     <>
       <section className="body-game">
         <header>
           <p>
-            تعداد حرکت : <span>40</span>
+            تعداد حرکت : <span>{move}</span>
           </p>
           <p>
-            زمان : <span>2:00</span>
+            زمان :<span>{gameTime}</span>
           </p>
         </header>
         <section className="body-game__items">
@@ -71,9 +125,11 @@ function App() {
           ))}
         </section>
         <footer>
-          <button>شروع دوباره</button>
+          <button onClick={handleStartGame}>شروع گیم</button>
+          <button onClick={handleStartAgain}>شروع دوباره</button>
         </footer>
       </section>
+      {move == 0 && <Gameover />}
     </>
   );
 }
---
+++ src/components/Gameover.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Gameover() {
+  return (
+    <div className='gameover'>Gameover you are noob!</div>
+  )
+}
+
+export default Gameover
